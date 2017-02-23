a2033c8b126d194df539ffe4f48ef477b80c1a50
x <powturbo@gmail.com>
2017-02-23 11:14:44 +0100

    Benchmark app

---
+++ tpbench.c
@@ -0,0 +1,187 @@
+/**
+    Copyright (C) powturbo 2013-2017
+    GPL v2 License
+  
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    - homepage : https://sites.google.com/site/powturbo/
+    - github   : https://github.com/powturbo
+    - twitter  : https://twitter.com/powturbo
+    - email    : powturbo [_AT_] gmail [_DOT_] com
+**/
+#include <stdlib.h>
+#include <stdio.h>
+#include <x86intrin.h>
+#ifdef __APPLE__
+#include <sys/malloc.h>
+#else
+#include <malloc.h>
+#endif
+  #ifdef _MSC_VER
+#include "vs/getopt.h"
+  #else
+#include <getopt.h> 
+#endif
+
+#include "conf.h"
+//#define RDTSC_ON
+#include "time_.h"
+  
+  #ifdef BITSHUFFLE
+#include "bitshuffle/src/bitshuffle.h"
+#include "bitshuffle/lz4/lz4.h"
+  #endif
+
+  #ifdef BLOSC
+#include "c-blosc/blosc/shuffle.h"
+#include "c-blosc/blosc/blosc.h"
+  #endif
+
+void memrcpy(unsigned char *out, unsigned char *in, unsigned n) { int i; for(i = 0; i < n; i++) out[i] = ~in[i]; }
+
+int memcheck(unsigned char *in, unsigned n, unsigned char *cpy) { 
+  int i;
+  for(i = 0; i < n; i++)
+    if(in[i] != cpy[i]) {
+      printf("ERROR in[%d]=%x, dec[%d]=%x\n", i, in[i], i, cpy[i]); 
+	  return i+1; 
+	}
+  return 0;
+}
+
+#ifdef LZ4_ON
+unsigned tp4lz4enc(unsigned char *in, unsigned n, unsigned char *out, unsigned esize, unsigned char *tmp) {
+  tp4enc(in, n, tmp, esize); 
+  return LZ4_compress(tmp, out, n);
+}
+
+unsigned tp4lz4dec(unsigned char *in, unsigned n, unsigned char *out, unsigned esize, unsigned char *tmp) {
+  unsigned rc; 
+  LZ4_decompress_fast((char *)in, (char *)tmp, n); 
+  tp4dec(tmp, n, (unsigned char *)out, esize);
+  return rc;
+}
+
+unsigned tplz4enc(unsigned char *in, unsigned n, unsigned char *out, unsigned esize, unsigned char *tmp) {
+  tpenc(in, n, tmp, esize); 
+  return LZ4_compress(tmp, out, n);
+}
+
+unsigned tplz4dec(unsigned char *in, unsigned n, unsigned char *out, unsigned esize, unsigned char *tmp) {
+  unsigned rc; 
+  LZ4_decompress_fast((char *)in, (char *)tmp, n); 
+  tpdec(tmp, n, (unsigned char *)out, esize);
+  return rc;
+}
+#endif
+
+#ifdef BITSHUFFLE
+#define   BITSHUFFLE(in,n,out,esize) bshuf_bitshuffle(  in, out, (n)/esize, esize, 0); memcpy((char *)out+((n)&(~(8*esize-1))),(char *)in+((n)&(~(8*esize-1))),(n)&(8*esize-1))
+#define BITUNSHUFFLE(in,n,out,esize) bshuf_bitunshuffle(in, out, (n)/esize, esize, 0); memcpy((char *)out+((n)&(~(8*esize-1))),(char *)in+((n)&(~(8*esize-1))),(n)&(8*esize-1))
+
+unsigned bslz4enc(unsigned char *in, unsigned n, unsigned char *out, unsigned esize, unsigned char *tmp) {
+  BITSHUFFLE(in, n, tmp, esize); 
+  return LZ4_compress(tmp, out, n);
+}
+
+unsigned bslz4dec(unsigned char *in, unsigned n, unsigned char *out, unsigned esize, unsigned char *tmp) {
+  unsigned rc; 
+  LZ4_decompress_fast((char *)in, (char *)tmp, n); 
+  BITUNSHUFFLE(tmp, n, (unsigned char *)out, esize);
+  return rc;
+}
+#endif
+
+int main(int argc, char* argv[]) {
+  unsigned trips = 15,cmp=0, b = 1 << 30, esize=4, lz=0, fno;
+  
+  int c, digit_optind = 0, this_option_optind = optind ? optind : 1, option_index = 0;
+  static struct option long_options[] = { {"blocsize", 	0, 0, 'b'}, {0,0, 0, 0}  };
+  for(;;) {
+    if((c = getopt_long(argc, argv, "b:ci:I:s:z", long_options, &option_index)) == -1) break;
+    switch(c) {
+      case  0 : printf("Option %s", long_options[option_index].name); if(optarg) printf (" with arg %s", optarg);  printf ("\n"); break;								
+      case 's': esize  = atoi(optarg);  break;
+      case 'i': tm_rep = atoi(optarg); if(!tm_rep) tm_rep=1,trips=1;  break;
+      case 'I': trips  = atoi(optarg); if(!trips) trips=1;  break;
+      case 'b': b = argtoi(optarg,1); 	break;
+      case 'z': lz++; 				  	break;
+      case 'c': cmp++; 				  	break;
+    }
+  }
+  if(argc - optind < 1) { fprintf(stderr, "File not specified\n"); exit(-1); }
+
+  unsigned char *in,*out,*cpy;
+  unsigned long long totlen=0,tot[3]={0};
+  for(fno = optind; fno < argc; fno++) {
+    char *inname = argv[fno];  									
+    FILE *fi = fopen(inname, "rb"); 							if(!fi ) { perror(inname); continue; } 							
+    fseek(fi, 0, SEEK_END); 
+    long long flen = ftell(fi); 
+	fseek(fi, 0, SEEK_SET);
+	
+    if(flen > b) flen = b;
+    int n = flen; 
+    if(!(in  =        (unsigned char*)malloc(n+1024)))        { fprintf(stderr, "malloc error\n"); exit(-1); } cpy = in;
+    if(!(out =        (unsigned char*)malloc(flen*4/3+1024))) { fprintf(stderr, "malloc error\n"); exit(-1); } 
+    if(cmp && !(cpy = (unsigned char*)malloc(n+1024)))        { fprintf(stderr, "malloc error\n"); exit(-1); }
+    n = fread(in, 1, n, fi);									printf("File='%s' Length=%u\n", inname, n);			
+    fclose(fi);
+    if(n <= 0) exit(0); 
+    if(fno == optind) {
+      tm_init(0, 0); 
+      TMBENCH("tpdec",tpdec(in,n,out,esize) ,n); // Warmup
+      tm_init(trips, 1);  
+      printf("size=%u, element size=%d. detected cpu=%d\n\n", n, esize, cpuini(0));
+    }		
+    if(!lz) {
+      TMBENCH("tpenc",tpenc(in, n,out,esize) ,n);					  	  TMBENCH("",tpdec(out,n,cpy,esize) ,n);  						memcheck(in,n,cpy); printf("\n");	  
+      TMBENCH("tp4enc",tp4enc(in,n,out,esize) ,n);      				  TMBENCH("",tp4dec(out,n,cpy,esize) ,n);  					memcheck(in,n,cpy); printf("\n");	   
+        #ifdef BLOSC
+      TMBENCH("blosc shuffle",shuffle(esize,n,in,out), n);				  TMBENCH("",unshuffle(esize,n,out,cpy), n); 					memcheck(in,n,cpy); printf("\n");	  
+        #endif
+        #ifdef BITSHUFFLE
+      TMBENCH("bitshuffle",bshuf_bitshuffle(in,out,(n)/esize,esize,0), n); TMBENCH("",bshuf_bitunshuffle(out,cpy,(n)/esize,esize,0), n);memcheck(in,n,cpy);printf("\n");	  
+        #endif
+	  TMBENCH("memcpy",memcpy(in,out,n) ,n);      printf("\n");
+    }
+    totlen += n;
+    // Test Transpose + lz	
+    char *tmp; int rc;   
+    if(!(tmp = (unsigned char*)malloc(n+1024))) { fprintf(stderr, "malloc error\n"); exit(-1); }
+      #ifdef LZ4_ON
+    memrcpy(cpy,in,n); TMBENCH("tpbyte+lz4",rc = tplz4enc(in, n,out,esize,tmp) ,n); tot[0]+=rc; TMBENCH("",tplz4dec(out,n,cpy,esize,tmp) ,n); memcheck(in,n,cpy);
+    printf("compressed len=%u ratio=%.2f\n", rc, (double)(rc*100.0)/(double)n); 
+    
+    memrcpy(cpy,in,n); TMBENCH("tpnibble+lz4",rc = tp4lz4enc(in, n,out,esize,tmp) ,n); tot[1]+=rc; TMBENCH("",tp4lz4dec(out,n,cpy,esize,tmp) ,n); memcheck(in,n,cpy);
+    printf("compressed len=%u ratio=%.2f\n", rc, (double)(rc*100.0)/(double)n);
+      #endif
+
+	  #ifdef BITSHUFFLE
+    //memrcpy(cpy,in,n); TMBENCH("bitshuffle+lz4",rc=bshuf_compress_lz4(in,out,n/esize,esize,0), n); tot[2] += rc; TMBENCH("",bshuf_decompress_lz4(out,cpy,n/esize,esize,0), n); memcheck(in,n,cpy);
+    memrcpy(cpy,in,n); TMBENCH("bitshuffle+lz4",rc=bslz4enc(in,n,out,esize,tmp), n); tot[2] += rc; TMBENCH("",bslz4dec(out,n,cpy,esize,tmp), n); memcheck(in,n,cpy);
+    printf("compressed len=%u ratio=%.2f\n", rc, (double)(rc*100.0)/(double)n);	
+      #endif
+    printf("\n");
+    free(tmp);
+  }
+    #ifdef HAVE_LZ4
+  printf("tplz4enc  :         compressed len=%llu ratio=%.2f %%\n", tot[0], (double)(tot[0]*100.0)/(double)totlen); 
+  printf("tp4lz4enc :         compressed len=%llu ratio=%.2f %%\n", tot[1], (double)(tot[1]*100.0)/(double)totlen); 
+    #endif
+    #ifdef BITSHUFFLE
+  printf("bshuf_compress_lz4: compressed len=%llu ratio=%.2f %%\n", tot[2], (double)(tot[2]*100.0)/(double)totlen); 
+    #endif
+}
